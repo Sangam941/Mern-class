4d8983ec6b24a1e42b9f7fe237b951281b097f14
Sangam Singh Dhami <sangamdhami663@gmail.com>
2026-08-23 22:03:31 +0545

    js operators, string, object

---
+++ day 12/script.js
@@ -0,0 +1,295 @@
+// console.log("hello")
+
+// let a = 9  //global variable
+
+// if(true){
+//     let a = 6
+//     console.log(a)  // inner value  6
+// }else{
+
+// }
+
+// console.log(a)  //outer value   9
+
+// // object
+
+// const obj = {
+//     "name":"ram",
+// }
+
+// obj.age = 23
+
+// const name = "hari"
+// name = "ram"
+
+// console.log(name)
+
+// // console.log(obj)
+
+
+// string
+
+// let a = "hello"
+// let b =  "world"
+
+// console.log(a + b) // concatination
+// console.log(a+ " "+b)
+
+
+// let c = "ram"
+// let d = "hari"
+// console.log(c + " and "+d + " play together in the garden")
+
+// // template leterals
+// console.log(`${c} and ${d} play together in the garden`)
+
+
+// let a = "Hello"
+// undefined
+// a
+// 'Hello'
+// a[3]
+// 'l'
+// a[4]
+// 'o'
+// a[0]
+// 'H'
+// a.toUpperCase()
+// 'HELLO'
+// a.toLowerCase()
+// 'hello'
+// a.replace("el","oo")
+// 'Hoolo'
+// undefined
+// a+b
+// 'Helloworld'
+// a.concat(b)
+// 'Helloworld'
+// a.slice(1,3)
+// 'el'
+// a
+// 'Hello'
+// a.slice(1)
+// 'ello'
+// let c = "       table   "
+// undefined
+// c
+// '       table   '
+// c.trim()
+// 'table'
+// a
+// 'Hello'
+// a.toUpperCase()
+// 'HELLO'
+// a
+// 'Hello'
+// a.startsWith('H')
+// true
+// a.startsWith('a')
+// false
+// a.endsWith('o')
+// true
+// a.endsWith('l')
+// false
+
+
+
+// truthy and falsy value
+
+// falsly value : 0 ,false, "", null, undefined, NaN, document.all
+
+// if(document.all){
+//     console.log("true")   // true block --> only runs if the condtion is true
+// }else{
+//     console.log("fasle")
+// }
+
+// true + false
+// 1
+// 1 + 0
+// 1
+// null + 1
+// 1
+// 0 + 1
+// 1
+// '5'+5
+// '55'
+// '5'-2
+// 3
+// '5'+5
+// '55'
+// 5+5
+// 10
+// '5'+5
+// '55'
+// '10'-3
+// 7
+
+
+// operators
+
+// Arithmetic operators:
+// + -> addition
+// - subtraction
+// / division
+// * multiplication
+// % modulo division
+// ** power/expoential
+
+// 5+5
+// 10
+// 7-4
+// 3
+// 10/2
+// 5
+// 5*3
+// 15
+// 10%2
+// 0
+// 2**3
+// 8
+
+
+// comparision operator
+
+/*
+==  comparision operator
+=== strickly comparision/tripple
+!=  not equal to
+!== strickly not equal to 
+>=  less then is equal to
+<=  greater than is equal to
+> less then
+<greater then
+
+*/
+
+// 5 = 5 
+// 5==5
+
+// 5 != '6'
+// true
+// 5 !== '6'
+// true
+// 5 != '5'
+// false
+// 5 !== '5'
+// true
+// 5 == '5'
+// true
+// 5 === '5'
+// false
+// false
+// false
+// !false
+// true
+// true
+// true
+// !true
+// false
+// !true
+// false
+// 5>3
+// true
+// 5<8
+// true
+// 5>5
+// false
+// 5>=5
+// true
+
+
+// Assignment
+//  = 
+//  +=
+//  -=
+//  *=
+//  /=
+//  %=
+
+
+// logical operator
+
+// && logical and
+// || logical or
+// !  logical not
+
+
+
+// if(3>2 && 3>5 ){
+//     console.log('A')
+// }else{
+//     console.log('B')
+// }
+
+// if(3>2 || 3>5 ){
+//     console.log('A')
+// }else{
+//     console.log('B')
+// }
+
+// if(!(3>2) || (3>5) ){
+//     console.log('A')
+// }else{
+//     console.log('B')
+// }
+
+
+// unary operator
+
+// +, - , !, typeof, ++, --
+
+
+
+
+// questions:
+
+// let a = 6
+// let b = 20
+
+// if(a>5 && b>25){
+//     console.log('A')
+// }else{
+//     console.log('B')
+// }
+
+
+// let isAdmin = true
+// let isLoggedIn = false
+
+// if(isAdmin || isLoggedIn){
+//     console.log('Access')
+// }else{
+//     console.log('denied')
+// }
+
+
+// let temprature = 40
+
+// if(!(temprature<40)){
+//     console.log('hot')
+// }else{
+//     console.log('average')
+// }
+
+
+// let a =0
+
+// if(a){
+//     console.log('truthy')
+// }else{
+//     console.log('fasly')
+// }
+
+
+
+// let x = 3
+// let y = x++
+
+// console.log(x,y)
+
+
+// let d = 6
+
+// console.log(d--)
+// console.log(d)
---
+++ day11 js/script.js
@@ -0,0 +1,120 @@
+// document.write("hello hello hele")
+// alert()
+console.log("starting js... here is day 11")
+
+// var
+// let
+// const
+
+// variable--> container
+
+// var a = 30
+// console.log(a)
+
+// data types: number, string, boolean, object, null, undefined
+// console.log(20)
+// console.log("hello there")
+// console.log(2==3)
+
+
+// comments
+/* aksdfhasd
+ askldfjasd
+ sakdfjs
+ */
+
+// typeof 
+
+// console.log(typeof(20))
+// console.log(typeof("hello"))
+// console.log(2==3)
+
+// // object   |  json formate
+// var a = {
+//     "name": "ram",
+//     "age": 40
+// }
+
+// console.log(a)
+
+
+//  var a =20   // functional scope
+// a=40
+//  console.log(a)
+
+//  let b=10     // block scope
+// b=50
+// console.log(b)
+
+// const c = 60    // block scope
+// const pi = 3.14
+// c=30
+// console.log(c)
+
+
+// if(5==5){
+//     let a = 5
+//     console.log(a)
+// }else{
+
+// }
+
+{
+    var d = 55 // it can be access from outside of the block
+    let b = 6 // block scope
+    const c=8 // block scope
+    console.log(b)
+}
+
+// console.log(b)
+// console.log(c)
+console.log(d)
+
+
+// functional scope
+// function sum(){
+//     {
+//         var r = 20 // functional scope
+//     }
+//     console.log(r)
+// }
+
+// sum()
+
+// console.log(r)
+
+
+// let a ;   //declaration
+// a=5  //initialize
+
+// let b=5  // initialize + decleration
+
+// console.log(a)
+
+
+// this is allowed
+// var a=5
+// var a=6
+//  console.log(a)
+
+
+//  this is not allowed
+//  let b =7
+//  let b=8 
+//  console.log(b)
+
+
+ // hoisting   - interviewer favourite qn
+
+ console.log("hoisting")
+ 
+ console.log(a)  // this is valid in var // undefined
+ var a   // declare
+
+
+ console.log(b)   // this is invalid in let and const  //refernce
+ let b  // declare
+
+
+
+
