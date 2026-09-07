eb40d06ed39b74358f5d276bd3d022c5a35ef56a
Sangam Singh Dhami <sangamdhami663@gmail.com>
2026-09-07 21:53:52 +0545

    day-21 usestate

---
+++ day21/react1/src/App.jsx
@@ -0,0 +1,77 @@
+
+// const App = () => {
+
+
+import Card from "./components/Card"
+
+//   const handleHello = (e)=>{
+//     console.log('hello', e)
+//   }
+
+//   const handleSubmit = (e)=>{
+//     e.preventDefault()
+//     console.log('submitted')
+//   }
+  
+//   return (
+//     <div className='h-screen bg-black text-white'>
+//       <button
+//       onClick={(e)=>{
+//         handleHello(e)
+//       }}
+//       className="px-4 py-3 bg-green-500 text-black cursor-pointer rounded-xl ">click me</button>
+
+//       <form onSubmit={(e)=>{
+//         handleSubmit(e)
+//       }} action="">
+//         <input type="text" />
+//         <input type="submit" />
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// const App = () => {
+//   const [number, setNumber] = useState(0) //initial value
+
+//   const handleNumber = ()=>{
+//     setNumber(number+1)
+//     console.log(number)
+//   }
+//   const handleNumber2 = ()=>{
+//     setNumber(number-1)
+//     console.log(number)
+//   }
+
+//   return (
+//     <div>
+//         <div>
+//           {number}
+//         </div>
+//       <button onClick={handleNumber}>
+//         +
+//       </button>
+//       <button onClick={handleNumber2}>
+//         -
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+const App = () => {
+  return (
+    <div className="h-screen bg-black text-white flex items-center justify-center">
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ day21/react1/src/components/Card.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+
+
+const Card = () => {
+
+    const [isFav, setIsFav] = useState(false)
+    const [quantity, setQuantity] = useState(1)
+
+    const handleToggle = ()=>{
+        setIsFav(!isFav)
+    }
+
+    // total = price x quantity 
+    const price = 500
+    const total = price * quantity
+
+  return (
+    <div className="h-[400px] w-[220px] bg-zinc-300 rounded-md overflow-hidden p-4 text-black">
+      <div className="image rounded-md overflow-hidden border-1 border-zinc-500">
+        <img src="https://images.unsplash.com/photo-1543163521-1bf539c55dd2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGlnaCUyMGhlZWxzfGVufDB8fDB8fHww" alt="this is high heels image" className="h-54"/>
+      </div>
+
+      <div className="below mt-4 flex flex-col gap-2">
+        <div className="title flex items-center justify-between">
+            <button className="text-xs px-2 py-1 border-1 border-zinc-500 rounded-md font-semibold cursor-pointer">Women Fashion</button>
+            <button 
+            onClick={handleToggle}
+            className={`favorite ${isFav?"text-red-500":"text-white"} cursor-pointer text-xl`}>❤︎</button>
+        </div>
+
+        <div className="content">
+            <h1 className="uppercase font-bold text-red-600">Elegent High Heels</h1>
+            <p className="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+            <div className="pricen flex items-center justify-between mt-2">
+                <div className="total">
+                    Rs. {total}
+                </div>
+
+                <div className="counter flex items-center gap-2">
+                    <button 
+                    onClick={()=>setQuantity(quantity-1)}
+                    className="sub px-2 cursor-pointer border-1 border-zinc-500">-</button>
+
+                    <div className="number">{quantity}</div>
+                    
+                    <button 
+                    onClick={()=>{setQuantity(quantity+1)}}
+                    className="sub px-2 cursor-pointer border-1 border-zinc-500">+</button>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
